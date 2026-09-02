213c3b918c5c89a5b6b10bef8897754efca4db6c
Ralph Denk <xgodxraven@MacBook-Air-von-Ralph.fritz.box>
2026-09-02 14:24:02 +0200

    Deploy Build 2026-09-02-8

--- app.js
+++ app.js
@@ -14774,7 +14774,7 @@ window.addEventListener('scroll',function(){ if(typeof updateFloatBtns==='functi
    Also: Die App prüft selbst, ob sie veraltet ist, und sagt es.
    ============================================================ */
 
-const APP_BUILD = "2026-09-02-7";
+const APP_BUILD = "2026-09-02-8";
 let _updateGezeigt = false;
 
 /* Produkteditor im Consumer nur bei echtem Admin-Bedarf nachladen. Im
