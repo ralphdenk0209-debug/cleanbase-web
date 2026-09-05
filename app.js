43dd06fd17ed059fd3ca932b887c524c41d4b2e8
Ralph Denk <ralph.denk@yahoo.de>
2026-09-05 06:06:29 +0200

    Deploy Build 2026-09-05-2

--- app.js
+++ app.js
@@ -15036,7 +15036,7 @@ window.addEventListener('scroll',function(){ if(typeof updateFloatBtns==='functi
    Also: Die App prüft selbst, ob sie veraltet ist, und sagt es.
    ============================================================ */
 
-const APP_BUILD = "2026-09-05-1";
+const APP_BUILD = "2026-09-05-2";
 let _updateGezeigt = false;
 
 /* Produkteditor im Consumer nur bei echtem Admin-Bedarf nachladen. Im
